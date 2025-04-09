8f67b3131e851a169a17faa5bbc2182478656b4c
marc1305 <marcus.v.olesen.02@gmail.com>
2025-04-09 10:38:56 +0200

    1

---
+++ Design Project-copy/Controller/LoanController.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import model.LoanContainer;
+import model.Loan;
+
+/**
+ * Lav en beskrivelse af klassen LoanCtrl her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class LoanController
+{
+    private LoanContainer loanContainer;
+    private FriendController friendController;
+    private LPController LPController;
+    private Loan loan;
+    
+    /**
+     * Konstruktør for objekter af klassen LoanCtrl
+     */
+    public LoanController() {
+        
+    }
+    
+    public void addCopyToLoan(Copy c) {
+        
+    }
+    
+    public void addToLoan(Loan l) {
+        
+    }
+    
+    public void setStatus() {
+        
+    }
+    
+    public void setBorrowedDate() {
+        
+    }
+}
+
