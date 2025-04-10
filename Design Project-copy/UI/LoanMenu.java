051de673cb7e9cf99f5233ec91b11cd4b6716cc0
JasEgal23 <egaljasmina@gmail.com>
2025-04-10 14:48:51 +0200

    Update LoanMenu.java

--- Design Project-copy/UI/LoanMenu.java
+++ Design Project-copy/UI/LoanMenu.java
@@ -1,68 +1,86 @@
 package UI;
+import java.util.Scanner;
 
 
 /**
- * Lav en beskrivelse af klassen LoanMenu her.
- * 
- * @author (dit navn her)
- * @version (versions nummer eller dato her)
+ * Write a description of class LoanMenu here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
  */
-public class LoanMenu
-{
-    // instansvariabler - erstat eksemplet herunder med dine egne variable
-
+public class LoanMenu{
     /**
-     * Konstruktør for objekter af klassen LoanMenu
+     * Constructor for objects of class LoanMenu
      */
-    public LoanMenu()
-    {
+    public LoanMenu() {
+        
+    }
+    public CreateLoan(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Phone Number");
+        String phone = getStringFromUser(keyboard);
+        
+        System.out.println("Serial Number");
+        int serialNumber = getIntergetFromUser(keyBoard);
+        
+        System.out.println("Loan Number");
+        int loanNumber = getIntergetFromUser(keyBoard);
+        
+        System.out.println("Borrowed Date");
+        String borrowedDate = getStringFromUser(keyboard);
+        
+        System.out.println("Return Date");
+        String returnDate = getStringFromUser(keyboard);
+        
+        System.out.println("Status");
+        Boolean status = getStringFromUser(keyboard);
+        
+        
+        LoanController loanController = new LoanController();
+        loanController.createLoan(phone, serialNumber, loanNumber, borrowedDate, returnDate, status);
+    
+    }
+    public void start() {
+        loanMenu();
+    }
+    private void loanMenu() {
         boolean running = true;
         while (running) {
             int choice = writeLoanMenu();
             switch (choice) {
                 case 1:
-                    
-                createLoan();
-                break;
-                
-                case 2:
-                    
-                addFriendByPhoneToLoan();
-                break;
-                
-                case 3:
-                    
-                addCopyBySerialNumberToLoan();
-                break;
-                
-                case 0: 
-                
-                System.out.println("Exit system, come agian");
-                
-                running = false;
-                break;
+                    createLoan();
+                    System.out.println("create loan");
+                    break;
+                case 0:
+                    running = false;
+                    break;
                 
             }
-    }
+        }
     }
     private int writeLoanMenu() {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("LOAD MENU");
-        System.out.println("1. createloan");
-        System.out.println("2. addFriendByPhoneToLoan");
-        System.out.println("3. addCopyBySerialNumberToLoan")
-        System.out.println("0. Exit system");
-        System.out.println("Take your pick");
-        return scanner.nextInt();
+        System.out.println("LOAN MENU");
+        System.out.println(" [1] create loan");
+        System.out.println(" [0] Exit system");
+        System.out.println("\n vælg");
+        int choice = getIntergetFromUser(keyboard);
+        return choice;
+  
     }
-    public void createLoan(){
-        
+    private int getIntergetFromUser(Scanner keyboard) {
+        while (!keyboard.hasNextInt()) {
+            System.out.println("Input only numbers");
+            keyboard.nextInt();
+        }
+        return keyboard.nextInt();
     }
-    
-    public addFriendByPhoneToLoan(String phone) {
-        return null;
-    } 
-    public addLPBySerialNumberToLoan() {
-        return null;
+    private int getStringFromUser(Scanner keyboard) {
+        while (!keyboard.hasNextLine()) {
+            System.out.println("Kun bogstaver");
+            keyboard.nextLine();
+        }
+        return keyboard.nextLine();
     }
 }
